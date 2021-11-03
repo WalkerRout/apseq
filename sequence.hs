7f109d0384a7a9ceb0f49bb4cbc11e8002bb23da
WalkerRout <walkerrout04@gmail.com>
2021-11-03 14:55:47 -0700

    Added header comment

--- sequence.hs
+++ sequence.hs
@@ -1,6 +1,8 @@
-{- 
+-- Title: APSequence
+-- Description: DNA sequence verfier with amino acid mapping 
+-- Author: Walker Rout
+-- Creation Date: Wednesday, Novemeber 3rd, 2021
 
--}
 
 module Nucleotide
 ( nucDict
@@ -16,7 +18,6 @@ module Nucleotide
 type Sequence = String
 
 
-
 nucDict s = case s of
   'A' -> 'T'
   'T' -> 'A'
@@ -24,20 +25,16 @@ nucDict s = case s of
   'G' -> 'C'
 
 
-
 aminoDict s = case s of
   
-
 
 verifySequence :: Sequence -> [Char]
 verifySequence s = [x | x <- s, x == 'A' || x == 'T' || x == 'C' || x == 'G']
 
 
-
 complementSequence :: Sequence -> [Sequence]
 complementSequence seq = [s, [nucDict x | x <- s]]
   where s = verifySequence seq
-
 
 
 main :: IO()
