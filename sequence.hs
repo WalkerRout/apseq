5cf7f0a5d5202467e69161b72da2100eb0cdd9f0
WalkerRout <walkerrout04@gmail.com>
2021-11-03 14:45:58 -0700

    Inital Commit

---
+++ sequence.hs
@@ -0,0 +1,47 @@
+{- 
+
+-}
+
+module Nucleotide
+( nucDict
+, verifySequence
+, complementSequence
+) where
+
+
+-- Data Initialization (N/A)
+
+
+-- Type Initialization
+type Sequence = String
+
+
+
+nucDict s = case s of
+  'A' -> 'T'
+  'T' -> 'A'
+  'C' -> 'G' 
+  'G' -> 'C'
+
+
+
+aminoDict s = case s of
+  
+
+
+verifySequence :: Sequence -> [Char]
+verifySequence s = [x | x <- s, x == 'A' || x == 'T' || x == 'C' || x == 'G']
+
+
+
+complementSequence :: Sequence -> [Sequence]
+complementSequence seq = [s, [nucDict x | x <- s]]
+  where s = verifySequence seq
+
+
+
+main :: IO()
+main = do
+  input <- getLine 
+  print $ complementSequence input
+
