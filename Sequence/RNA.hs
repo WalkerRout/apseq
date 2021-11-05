01960991a45f4b28103578dca9499bede177936f
WalkerRout <walkerrout04@gmail.com>
2021-11-05 14:25:19 -0700

    Added lowercase support

--- Sequence/RNA.hs
+++ Sequence/RNA.hs
@@ -12,7 +12,7 @@ import qualified Sequence.Dictionaries as Dict
 
 
 verifySequence :: Data.Sequence -> Data.Sequence
-verifySequence s = [x | x <- s, toLower x == 'a' || toLower x == 'u' || toLower  x == 'c' || toLower x == 'g']
+verifySequence s = [toUpper x | x <- s, toLower x == 'a' || toLower x == 'u' || toLower x == 'c' || toLower x == 'g']
 
 
 complementSequence :: Data.Sequence -> [Data.Sequence]
